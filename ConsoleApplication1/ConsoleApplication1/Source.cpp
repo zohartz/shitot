8ff7f0cb01fa39ce077b5ccb0fad9db15c76f4bc
zohartz <tz.zohar@gmail.com>
2016-02-25 15:31:32 +0200

    bjhnjnjnji

---
+++ ConsoleApplication1/ConsoleApplication1/Source.cpp
@@ -0,0 +1,19 @@
+// shitot1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdio.h"
+#include "stdlib.h"
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+
+int main(int argc, char argv[])
+{
+
+	//printf("jjskkksan");
+	cout << "hello" << endl;
+	return 0;
+}
+
